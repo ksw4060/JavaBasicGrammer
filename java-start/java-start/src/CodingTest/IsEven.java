21d67d00425a49bbfb16f51b699ded36e6fcbba6
ksw4060 <gokorea1214@kakao.com>
2023-12-12 23:52:25 +0900

    조건문 연습문제 전까지

---
+++ java-start/java-start/src/CodingTest/IsEven.java
@@ -0,0 +1,14 @@
+package CodingTest;
+import java.util.Scanner;
+
+public class IsEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 ==0 ) {
+            System.out.println(n+" is odd");
+        } else {
+            System.out.println(n+" is even");
+        }
+    }
+}
